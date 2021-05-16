d6bc6d0d81245ef34c84d2240489520bf14b6b68
roksang <lroky@naver.com>
2021-05-16 23:08:52 +0900

    spring boot - board project

---
+++ src/main/java/rokrok/board/BoardApplication.java
@@ -0,0 +1,18 @@
+package rokrok.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+@ComponentScan(basePackages={"controller"})
+@ComponentScan(basePackages={"config"})
+public class BoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardApplication.class, args);
+	}
+
+}
